5678b0c7ce0afa927edd5219a4afd116abf89662
Sasha <natson6221@mail.ru>
2019-04-01 10:22:46 +0300

    MyFirstSave

--- Практикческое занятие 1/Практикческое занятие 1/Program.cs
+++ Практикческое занятие 1/Практикческое занятие 1/Program.cs
@@ -10,6 +10,45 @@ namespace Практикческое_занятие_1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Задача ввести нужную собаку с клавиатуры");
+            User sasha = new User();
+            sasha.GetInfo();
+        }
+
+
+        class User
+        {
+            public string Name { get; set; }
+            public String Poroda { get; set; }
+            public double Age { get; set; }
+            public User()
+            {
+                Name = null;
+                Poroda = null;
+                Age = 0;
+
+                SetInfo();
+            }
+
+            public void SetInfo()
+            {
+                Console.WriteLine("Введите имя собаки :");
+                Name = Console.ReadLine();
+
+                Console.WriteLine("Введите породу собаки ");
+                Poroda = Console.ReadLine();
+                Console.WriteLine("Введите возраст собаки");
+                Age = double.Parse(Console.ReadLine());
+            }
+
+            public void GetInfo()
+            {
+                Console.WriteLine($"Имя: {Name}");
+                Console.WriteLine($"Порода: {Poroda}");
+                Console.WriteLine($"Возраст: {Age}");
+                Console.ReadKey();
+            }
         }
     }
 }
+
