311915cece60abffde1a4cc3d0d217a2c44a9624
Sasha <natson6221@mail.ru>
2019-04-01 11:51:23 +0300

    LOX

--- Практикческое занятие 1/Практикческое занятие 1/Program.cs
+++ Практикческое занятие 1/Практикческое занятие 1/Program.cs
@@ -12,48 +12,10 @@ namespace Практикческое_занятие_1
         {
             Console.WriteLine("Задача ввести нужную собаку с клавиатуры");
             User sasha = new User();
+            sasha.SetInfo();
             sasha.GetInfo();
         }
 
-        class User
-        {
-            public string Name { get; set; }
-            public String Poroda { get; set; }
-            public double Age { get; set; }
-            public User()
-            {
-                Name = null;
-                Poroda = null;
-                Age = 0;
-
-                SetInfo();
-            }
-            public User(string name, string poroda, double age)
-            {
-                Name = name;
-                Poroda = poroda;
-                Age = age;
-            }
-            public void SetInfo()
-            {
-                Console.WriteLine("Введите имя собаки :");
-                Name = Console.ReadLine();
-
-                Console.WriteLine("Введите породу собаки ");
-                Poroda = Console.ReadLine();
-                Console.WriteLine("Введите возраст собаки");
-                Age = double.Parse(Console.ReadLine());
-            }
-
-
-            public void GetInfo()
-            {
-                Console.WriteLine($"Имя: {Name}");
-                Console.WriteLine($"Порода: {Poroda}");
-                Console.WriteLine($"Возраст: {Age}");
-                Console.ReadKey();
-            }
-        }
     }
 }
 
---
+++ Практикческое занятие 1/Практикческое занятие 1/User.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практикческое_занятие_1
+{
+    class User
+    {
+        public string Name { get; set; }
+        public string Poroda { get; set; }
+        public double Age { get; set; }
+
+        public User()
+        {
+            Name = null;
+            Poroda = null;
+            Age = 0;
+        }
+
+        public User(string name, string poroda, double age)
+        {
+            Name = name;
+            Poroda = poroda;
+            Age = age;
+        }
+
+        public void SetInfo()
+        {
+            Console.WriteLine("Введите имя собаки :");
+            Name = Console.ReadLine();
+
+            Console.WriteLine("Введите породу собаки ");
+            Poroda = Console.ReadLine();
+            Console.WriteLine("Введите возраст собаки");
+            Age = double.Parse(Console.ReadLine());
+        }
+
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Имя: {Name}");
+            Console.WriteLine($"Порода: {Poroda}");
+            Console.WriteLine($"Возраст: {Age}");
+            Console.ReadKey();
+        }
+    }
+}
